9e0e423513891eae2f9cbb2202ed3e03cc9eae7a
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-08 15:08:03 -0600

    add handlebar helper file to help with template formatting

---
+++ helpers/hbs.js
@@ -0,0 +1,50 @@
+const moment = require('moment');
+
+module.exports = {
+  formatDate: function (date, format) {
+    return moment(date).format(format);
+  },
+
+  truncate: function (str, len) {
+    if (str.length > len && str.length > 0) {
+      let new_str = str + ' ';
+      new_str = str.substr(0, len);
+      new_str = str.substr(0, new_str.lastIndexOf(' '));
+      new_str = new_str.length > 0 ? new_str : str.substr(0, len);
+      return new_str + '...';
+    }
+  },
+
+  stripTags: function (input) {
+    return input.replace(/<(?:.|\n)*?>/gm, '');
+  },
+
+  editIcon: function (
+    observationUser,
+    loggedUser,
+    observationId,
+    floating = true
+  ) {
+    if (observationUser._id.toString() == loggedUser._id.toString()) {
+      if (floating) {
+        return `<a href="/observations/edit/${observationId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`;
+      } else {
+        return `<a href="/observations/edit/${observationId}"><i class="fas fa-edit"></i></a>`;
+      }
+    } else {
+      return '';
+    }
+  },
+  select: function (selected, options) {
+    return options
+      .fn(this)
+      .replace(
+        new RegExp(' value="' + selected + '"'),
+        '$& selected="selected"'
+      )
+      .replace(
+        new RegExp('>' + selected + '</option>'),
+        ' selected="selected"$&'
+      );
+  },
+};
