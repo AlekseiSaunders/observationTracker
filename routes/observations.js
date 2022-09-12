7fe5be69c2a507094a4103868fd073307abb6c92
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-12 15:29:11 -0600

    add observation controller for all observation routes

--- routes/observations.js
+++ routes/observations.js
@@ -2,150 +2,52 @@ const { request } = require('express');
 const express = require('express');
 const router = express.Router();
 const { ensureAuth } = require('../middleware/auth');
+const observationController = require('../controllers/observation');
 
 const Observation = require('../models/Observation');
 
-// @desc Show all stories
-// @route GET /stories
+// @desc Show all observations
+// @route GET /observations
 
-router.get('/', ensureAuth, async (req, res) => {
-  try {
-    const observations = await Observation.find({ status: 'public' })
-      .populate('user')
-      .sort({ createdAt: 'desc' })
-      .lean();
-    res.render('observations/index', { observations });
-  } catch (err) {
-    console.error(err);
-    res.render('error/500');
-  }
-});
+router.get('/', ensureAuth, observationController.getObservations);
 
 // @desc Show add page
-// @route GET /stories/add
+// @route GET /observations/add
 
-router.get('/add', ensureAuth, (req, res) => {
-  res.render('observations/add');
-});
+router.get('/add', ensureAuth, observationController.addObservation);
 
-// @desc Show single story
-// @route GET /stories/:id
+// @desc Show single observation
+// @route GET /observations/:id
 
-router.get('/:id', ensureAuth, async (req, res) => {
-  try {
-    let observation = await Observation.findById(req.params.id)
-      .populate('user')
-      .lean();
-    if (!observation) {
-      return res.render('error/404');
-    }
-    res.render('observations/show', {
-      observation,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.render('error/404');
-  }
-});
+router.get('/:id', ensureAuth, observationController.showSingleObservation);
 
 // @desc Process add form
-// @route POST /stories
+// @route POST /observations
 
-router.post('/', ensureAuth, async (req, res) => {
-  try {
-    req.body.user = req.user.id;
-    await Observation.create(req.body);
-    res.redirect('/dashboard');
-  } catch (err) {
-    console.error(err);
-    res.render('error/500');
-  }
-});
+router.post('/', ensureAuth, observationController.processObservation);
 
 // @desc Show edit page
-// @route GET /stories/edit/:id
+// @route GET /observations/edit/:id
 
-router.get('/edit/:id', ensureAuth, async (req, res) => {
-  try {
-    const observation = await Observation.findOne({
-      _id: req.params.id,
-    }).lean();
-    if (!observation) {
-      return res.render('error/404');
-    }
-    if (observation.user != req.user.id) {
-      res.redirect('/observation');
-    } else {
-      res.render('observations/edit', {
-        observation,
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    return res.render('error/500');
-  }
-});
+router.get('/edit/:id', ensureAuth, observationController.editObservation);
 
-// @desc Update story
-// @route PUT /stories/:id
+// @desc Update observation
+// @route PUT /observations/:id
 
-router.put('/:id', ensureAuth, async (req, res) => {
-  try {
-    let observation = await Observation.findById(req.params.id).lean();
-    if (!observation) {
-      return res.render('error/404');
-    }
-    if (observation.user != req.user.id) {
-      res.redirect('/observations');
-    } else {
-      observation = await Observation.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body,
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-      res.redirect('/dashboard');
-    }
-  } catch (error) {
-    console.error(error);
-    return res.redirect('error/500');
-  }
-});
+router.put('/:id', ensureAuth, observationController.updateObservation);
 
-// @desc Delete story
-// @route DELETE /stories/:id
+// @desc Delete observation
+// @route DELETE /observation/:id
 
-router.delete('/:id', ensureAuth, async (req, res) => {
-  try {
-    await Observation.deleteOne({ _id: req.params.id });
-    res.redirect('/dashboard');
-  } catch (error) {
-    console.error(error);
-    return res.render('error/500');
-  }
-});
+router.delete('/:id', ensureAuth, observationController.deleteObservation);
 
-// @desc Show user stories
-// @route GET /stories/user/:userId
+// @desc Show user observations
+// @route GET /observations/user/:userId
 
-router.get('/user/:userId', ensureAuth, async (req, res) => {
-  try {
-    const observations = await Observation.find({
-      user: req.params.userId,
-      status: 'public',
-    })
-      .populate('user')
-      .lean();
-
-    res.render('observations/index', {
-      observations,
-    });
-  } catch (error) {
-    console.error(error);
-    res.redirect('error/500');
-  }
-});
+router.get(
+  '/user/:userId',
+  ensureAuth,
+  observationController.showUserObservations
+);
 
 module.exports = router;
