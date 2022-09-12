0d71c0644dad641219523cca188498cf403c1e3a
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-12 14:21:29 -0600

    add methodOverride package to allow put and delete functionality

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const methodOverride = require('method-override');
 const MongoStore = require('connect-mongo');
 
 const path = require('path');
@@ -20,6 +21,17 @@ const observationRoutes = require('./routes/observations');
 // Allows getting data from request.body
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 // Load dotenv config
 dotenv.config({ path: './config/config.env' });
