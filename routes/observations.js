f4694fd603ee384f6e5dfe3990277b58effcde9d
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-19 15:34:32 -0600

    add image upload to observation route

--- routes/observations.js
+++ routes/observations.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middleware/multer');
 const { ensureAuth } = require('../middleware/auth');
 const observationController = require('../controllers/observation');
 
@@ -11,17 +12,22 @@ router.get('/', ensureAuth, observationController.getObservations);
 // @desc Show add page
 // @route GET /observations/add
 
+router.post(
+  '/',
+  upload.single('file'),
+  ensureAuth,
+  observationController.processObservation
+);
+
+// @desc Process add form
+// @route POST /observations
+
 router.get('/add', ensureAuth, observationController.addObservation);
 
 // @desc Show single observation
 // @route GET /observations/:id
 
 router.get('/:id', ensureAuth, observationController.showSingleObservation);
-
-// @desc Process add form
-// @route POST /observations
-
-router.post('/', ensureAuth, observationController.processObservation);
 
 // @desc Show edit page
 // @route GET /observations/edit/:id
